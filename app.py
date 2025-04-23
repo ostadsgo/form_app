9b894808c19410906ae0870320a4ccf136fbbc83
Saeed Gholami <saeed.ghollami@gmail.com>
2025-04-23 05:25:22 +0330

    Create menubar and load first form on main frame.

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 from views.main import MainWindow
-from PySide6.QtWidgets import QApplication
-
-import sys
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+    MainWindow.run()
+   
--- views/main.py
+++ views/main.py
@@ -1,40 +1,96 @@
+import sys
 from pathlib import Path
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QFrame, QVBoxLayout, QPushButton
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QAction 
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import QFile
+
 ### Global variables
 UI_DIR = Path(__file__).parent.parent / "ui"
 
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
-        # super().__init__()
-        #
-        # # Load ui
-        # loader = QUiLoader()
-        # self.ui = loader.load(UI_DIR / "app2.ui", self)
-        # print(UI_DIR / "app.ui")
-        # self.setCentralWidget(self.ui)
-        # # self.resize(600, 500)
-        # #
-        # # self.setWindowTitle("hello world!")
-        # print(self.ui.pushButton)
         super().__init__()
+        self.init()
+        self.layout = QVBoxLayout()
+
+        # Menubar
+        self.menubar()
+        self.main()
+        self.statusbar()
+
+
+    def init(self):
+        self.setLayoutDirection(Qt.RightToLeft)
+        self.setWindowTitle("Hesab System")
+        self.resize(800, 600)
+
+    def menubar(self):
+        menubar = self.menuBar()
+        menubar.setLayoutDirection(Qt.RightToLeft)
+
+        ### Form menu
+        form_menu = menubar.addMenu("فرم")
+        form_create_action = QAction("ایجاد فرم", self)
+        form_update_action = QAction("ویرایش فرم", self)
+        form_delete_action = QAction("حذف فرم", self)
+        form_menu.addActions([form_create_action, form_update_action, form_delete_action])
+
+        ### Information menu
+        info_menu = menubar.addMenu("اطلاعات")
+        info_insert_action = QAction("افزودن اطلاعات", self)
+        info_display_action = QAction("نمایش اطلاعات", self)
+        info_menu.addActions([info_insert_action, info_display_action])
         
-        # Load the UI file
+        ### Report Menu
+        report_menu = menubar.addMenu("گزارشات")
+        report_yearly_action = QAction("گزارش ماهانه", self)
+        report_menu.addAction(report_yearly_action)
+
+        ### Help Menu
+        help_menu = menubar.addMenu("راهنمایی")
+        help_action = QAction("مستندات برنامه", self)
+        help_contact_action = QAction("تماس با ما", self)
+        help_about_action = QAction("درباره برنامه", self)
+        help_menu.addActions([help_action, help_contact_action, help_about_action])
+
+        ### EVENTS
+        form_create_action.triggered.connect(self.load_form_create_ui)
+
+
+    def main(self):
+        mainframe = QFrame()
+        mainframe.setFrameShape(QFrame.StyledPanel)
+        mainframe.setLineWidth(1)
+
+        mainframe.setLayout(self.layout)
+        self.setCentralWidget(mainframe)
+
+    def statusbar(self):
+        pass
+
+    def load_ui(self, filename):
         loader = QUiLoader()
-        file = QFile(UI_DIR / "app.ui")  # Your UI file path
-        file.open(QFile.ReadOnly)
-        self.ui = loader.load(file, self)
-        file.close()
-        
-        # Set up the main window
-        self.setCentralWidget(self.ui)
-        self.setWindowTitle("My Application")
+        ui = loader.load(UI_DIR / filename, self)
+        return ui
 
-        self.ui.action.triggered.connect(self.on_create_form)
+    def load_form_create_ui(self):
+        # Destroy form that is in main layout
+        self.layout.takeAt(0)
+
+        ui = self.load_ui("form_create.ui")
+        self.layout.addWidget(ui)
 
 
-    def on_create_form(self):
-        print('hello')
+    @classmethod
+    def run(self):
+        app = QApplication(sys.argv)
+        window = MainWindow()
+        window.show()
+        sys.exit(app.exec())
+
+
+
 
